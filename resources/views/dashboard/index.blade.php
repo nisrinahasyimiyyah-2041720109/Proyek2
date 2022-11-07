aece4c711536185656e06ff85b1c78b58a4ec0d2
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2022-11-07 09:31:13 +0700

    responsive bg

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -39,7 +39,7 @@
                 </div>
               </div> 
                 <div>
-                  <img src="{{ asset('images\bg-cute.jpeg') }}" height="590" width="1400">
+                  <img src="{{ asset('images\bg-cute.jpeg') }}" height="auto" width="100%">
                 </div>
             </div>
           </div>
