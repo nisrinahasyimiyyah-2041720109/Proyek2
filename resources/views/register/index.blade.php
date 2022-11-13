c88cf00ec2afda6cc2f806213b150e5da2ade3f0
tzelginianc <tzelginia78@gmail.com>
2022-11-13 17:46:43 +0700

    menambahkan if

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -2,6 +2,19 @@
 @section('content')
 
 <div class="login-dark" style="height: 695px;">
+    @if (session()->has('loginError'))
+        {{-- <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          {{ session('loginError') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div> --}}
+        <script>
+          $(document).ready(function(){
+            $(".modal-title").text("Login Gagal!");
+            $(".modal-body p").text("{{ session('loginError') }}");
+            $("#myModal").modal('show');
+          });
+        </script>
+        @endif
     <form action="/register" method="post">
         @csrf
         <h2 class="sr-only">Register Form</h2>
@@ -51,6 +64,21 @@
             <button class="btn btn-primary btn-block" type="submit">Register</button>
         </div>
         <a class="forgot" href="/login">Have any account?</a></form>
+<div id="myModal" class="modal fade ">
+  <div class="modal-dialog">
+      <div class="modal-content ">
+          <div class="modal-header bg-warning">
+              <h5 class="modal-title"></h5>
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+          <div class="modal-body">
+      <p></p>
+      
+          </div>
+      </div>
+  </div>
+</div>
+
 </div>
     
 @endsection
