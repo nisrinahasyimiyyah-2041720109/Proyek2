9d4f610f65921e9b4ed17a4311e4d89c2f2f2cb0
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2022-11-19 18:11:34 +0700

    landing page belom selesai

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,45 @@
 @extends('layout.main')
 
 @section('content')
-    
-<div class="row">
+
+<!-- Carousel Start -->
+<div class="container-fluid p-0 mb-12">
+        <div class="owl-carousel header-carousel position-fixed">
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" src="images/background.png" alt="">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(24, 29, 56, .7);">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-sm-10 col-lg-8">
+                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">Lembaga Bimbingan Belajar Terbaik</h5>
+                                <h1 class="display-3 text-white animated slideInDown">Fantastic Smart Institute</h1>
+                                <p class="fs-5 text-white mb-4 pb-2">Fantastic Smart Institute merupakan lembaga bimbingan belajar untuk siswa SD
+                                hingga SMP yang berada di Kecamatan Kedungpring, Kabupaten Lamongan, Jawa Timur</p>
+                                @auth
+                                    @if(auth()->user()->role == "member" )
+                                        @if(auth()->user()->transaksi->count() == 0)
+                                            <p class="fs-5 text-white mb-4 pb-2">
+                                                Anda belum terdaftar bimbel. Klik menu "Bimbel" untuk memilih kelas bimbel yang tersedia.
+                                            </p>
+                                        @else
+                                            <p class="fs-5 text-white mb-4 pb-2">
+                                                Anda telah terdaftar pada bimbel : {{ $transaksi->course->title }}<br>
+                                                Klik menu "My Class" untuk melihat kelas bimbel Anda.
+                                            </p>
+                                        @endif
+                                    @endif
+                                @endauth
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- Carousel End -->
+
+
+<!-- <div class="row">
     <div class="col ">
         <div class="profile">
             <h1 class="fw-bold">FAST</h1>
@@ -13,19 +50,19 @@
             @auth
                 @if(auth()->user()->role == "member" )
                     @if(auth()->user()->transaksi->count() == 0)
-                        <h4>
+                        <h3>
                             Anda belum terdaftar bimbel. Klik menu "Bimbel" untuk memilih kelas bimbel yang tersedia.
-                        </h4>
+                        </h3>
                     @else
-                        <h4>
+                        <h3>
                            
                             Anda telah terdaftar pada bimbel : {{ $transaksi->course->title }}<br><br>
                             Klik menu "My Class" untuk melihat kelas bimbel Anda.
-                        </h4>
+                        </h3>
                     @endif
                 @endif
             @endauth
         </div>
     </div>
-</div>
+</div> -->
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -11,6 +11,29 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
     <link rel="stylesheet" href="{{ asset('vendors/mdi/css/materialdesignicons.min.css') }}">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('css/new/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('css/new/style.css') }}" rel="stylesheet">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   </head>
   <body>
@@ -38,7 +61,16 @@
           }
           
         </script>
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
   </div>
   </body>
 </html>
