eb26a058149f168023760e8604e62ab66c547ab6
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2022-12-03 15:55:36 +0700

    benerkan tabel tugas

--- resources/views/dashboard/tugas/index.blade.php
+++ resources/views/dashboard/tugas/index.blade.php
@@ -3,7 +3,7 @@
    
 
   
-  <div class="table-responsive col-lg-6 mx-5 mt-4">
+  <div class="table-responsive col-lg-8 mx-5 mt-4">
     {{-- <a href="/admin/tugas/create" class="btn btn-primary mb-3">Tambah tugas</a> --}}
     <table class="table table-striped table-sm">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
